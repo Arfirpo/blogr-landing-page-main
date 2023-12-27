bd4fb665c3bf1dd3cf5297d0179aae2a9151a6a7
Agustin Firpo <agustin_@outlook.com.ar>
2023-12-27 03:25:02 -0300

    header dready

--- src/components/MenuNav.jsx
+++ src/components/MenuNav.jsx
@@ -24,8 +24,8 @@ export const MenuNav = () => {
   return (
     <div className="flex flex-col md:flex-row md:max-w-[310px] items-center justify-center gap-7 text-center text-lg  font-bold w-full ">
       {Object.values(menu).map((menuItem, index) => (
-        <div key={index} className="w-full h-full flex flex-col gap-4 ">
-          <div className="flex items-center justify-center gap-2">
+        <div key={index} className="w-full h-full flex flex-col gap-4 md:relative">
+          <div className="flex items-center justify-center gap-2 ">
             <h3
               className="text-veryDarkBlue cursor-pointer md:text-White"
               onClick={() => handleHeadingClick(menuItem.heading)}
@@ -41,7 +41,7 @@ export const MenuNav = () => {
             />
           </div>
           {openItem === menuItem.heading && (
-            <ul className="bg-[#ebeaef] w-full h-full flex flex-col justify-evenly rounded-lg py-6 gap-5">
+            <ul className="bg-[#ebeaef] md:absolute w-full h-full md:w-[220%] md:h-max md:top-[2.5rem] md:-left-8 flex flex-col justify-evenly md:justify-center md:gap-2 items-start pl-8 rounded-lg py-6 gap-5">
               {menuItem.items.map((item, subIndex) => (
                 <li
                   className="text-[.93rem] text-veryDarkGrayishBlue cursor-pointer hover:text-veryDarkBlue hover:text-[0.97rem]"

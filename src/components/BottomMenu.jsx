016127859f970c8e53d27319900e5a7c01060a91
Agustin Firpo <agustin_@outlook.com.ar>
2023-12-28 02:37:10 -0300

    continue w/ main and footer

--- src/components/BottomMenu.jsx
+++ src/components/BottomMenu.jsx
@@ -6,7 +6,7 @@ import { menu } from "../App";
 
 export const BottomMenu = () => {
   return (
-    <div className="w-[28%] h-[69%] flex flex-col justify-between">
+    <div className="w-[28%] h-[69%] flex flex-col justify-between xl:justify-center xl:gap-[12.375rem] xl:flex-row xl:w-[60%] xl:items-start">
       {Object.keys(menu).map((key) => (
         <SubMenuBottom key={key} title={menu[key].heading} items={menu[key].items} />
       ))}
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ import { BottomMenu } from "./BottomMenu";
 
 export const Footer = () => {
   return (
-    <footer className="border-[2px] border-grayishBlue w-full h-[56.25rem] bg-veryDarkGrayBlue text-center text-grayishBlue rounded-tr-[7.5rem] flex flex-col items-center justify-evenly">
-      <img src={logo} alt="footer logo" />
+    <footer className="border-[2px] border-grayishBlue w-full h-[56.25rem] xl:h-[30rem] bg-veryDarkGrayBlue text-center text-grayishBlue rounded-tr-[7.5rem] flex flex-col items-center justify-evenly xl:flex-row xl:relative">
+      <img src={logo} alt="footer logo" className="xl:self-start xl:absolute xl:top-20 xl:left-24" />
       <BottomMenu />
     </footer>
   );

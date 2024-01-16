fea150cb7ce326b6de9f7efdee02dfd33619ee5c
Agustin Firpo <agustin_@outlook.com.ar>
2024-01-15 21:05:52 -0300

    challenge finished 1.0

--- src/components/SubMenuBottom.jsx
+++ src/components/SubMenuBottom.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 export const SubMenuBottom = ({ title, items }) => {
   return (
-    <div className='font-Ubuntu text-[1rem] flex flex-col gap-[30px]'>
+    <div className='font-Ubuntu text-[1rem] flex flex-col gap-[1.875rem] xl:gap-[2.5rem] xl:text-left'>
       <h2 className='text-White font-semibold cursor-pointer'>{title}</h2>
-      <ul className='flex flex-col gap-[.3rem]'>
+      <ul className='flex flex-col gap-[.3rem] xl:gap-5'>
         {items.map((item, index) => (
-          <li className='text-[1.1rem] cursor-pointer hover:underline' key={index}>{item}</li>
+          <li className='text-[1.1rem] cursor-pointer hover:underline text-grayishBlue' key={index}>{item}</li>
         ))}
       </ul>
     </div>
